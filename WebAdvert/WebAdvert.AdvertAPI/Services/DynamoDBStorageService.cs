2d06d8abf0298394b9663d726a6b4e0bc5a156e2
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-30 08:29:56 -0600

    Added Service and Nuget: DynamoDBAwsV2

---
+++ WebAdvert/WebAdvert.AdvertAPI/Services/DynamoDBStorageService.cs
@@ -0,0 +1,21 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.AdvertAPI.Services
+{
+    public class DynamoDBStorageService : IAdvertStorageService
+    {
+        public Task<string> Add(AdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
