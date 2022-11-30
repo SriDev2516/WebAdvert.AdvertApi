683d5accd8b276dd5d69558ba8c3561aa602f0bc
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-30 08:36:32 -0600

    Added AdvertDbModel for mapping and saving data to the DB

---
+++ WebAdvert/WebAdvert.AdvertAPI/Services/AdvertDbModel.cs
@@ -0,0 +1,26 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.AdvertAPI.Services
+{
+    [DynamoDBTable("Adverts")]
+    public class AdvertDbModel
+    {
+        [DynamoDBProperty]
+        public int Id { get; set; }
+        [DynamoDBProperty]
+        public string Title { get; set; }
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        [DynamoDBProperty]
+        public DateTime CreationDateTime { get; set; }
+        [DynamoDBProperty]
+        public AdvertStatus Status { get; set; }
+    }
+}
--- WebAdvert/WebAdvert.AdvertAPI/Services/IAdvertStorageService.cs
+++ WebAdvert/WebAdvert.AdvertAPI/Services/IAdvertStorageService.cs
@@ -1,7 +1,4 @@
 ﻿using AdvertApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAdvert.AdvertAPI.Services
