fd5975266b3ecaccd0ef8bdfa96982fae7307b24
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-30 09:05:51 -0600

    Added Dependency for IAdvertStorageService in the StartUp.cs

--- WebAdvert/WebAdvert.AdvertAPI/Startup.cs
+++ WebAdvert/WebAdvert.AdvertAPI/Startup.cs
@@ -1,16 +1,10 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebAdvert.AdvertAPI.Services;
 
 namespace WebAdvert.AdvertAPI
 {
@@ -28,6 +22,7 @@ namespace WebAdvert.AdvertAPI
         {
             services.AddControllers();
             services.AddAutoMapper();
+            services.AddTransient<IAdvertStorageService, DynamoDBStorageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
