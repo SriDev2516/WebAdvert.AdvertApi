e8b58f842f0d4b94308ad9580e716eb9dde3330a
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-30 08:45:19 -0600

    Created Automapper and added nuget: AutoMapper.Extension.DependencyInjection Created Advert profile

--- WebAdvert/WebAdvert.AdvertAPI/Startup.cs
+++ WebAdvert/WebAdvert.AdvertAPI/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace WebAdvert.AdvertAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
