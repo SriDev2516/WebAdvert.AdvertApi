2552eccc6a5ef3284eef98cd4e82fc0324d9c97a
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-12-01 00:21:53 -0600

    Created Advert Controller and Added Create Method.

---
+++ WebAdvert/WebAdvert.AdvertAPI/Controllers/AdvertController.cs
@@ -0,0 +1,46 @@
+﻿using AdvertApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert.AdvertAPI.Services;
+
+namespace WebAdvert.AdvertAPI.Controllers
+{
+    [Route("adverts/v1")]
+    [ApiController]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService advertStorageService;
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            this.advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId = string.Empty;
+            try
+            {
+                recordId = await advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                throw;
+            }
+
+            return StatusCode(201, new CreateAdvertResponse() { Id = recordId });
+        }
+    }
+}
